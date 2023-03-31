ef9034e5ee7986080d3f415c4c1ef4d3e5c11fdf
smgsankar <gomzsankar@gmail.com>
2023-03-31 14:31:32 +0530

    fix(notifications-page): add cursor pointer to message card

--- challenges/notifications-page/src/components/MessageCard.tsx
+++ challenges/notifications-page/src/components/MessageCard.tsx
@@ -4,7 +4,7 @@ interface MessageCardPropsType{
 
 export function MessageCard(props: MessageCardPropsType) {
   return (
-    <p className="p-4 border-grayish-blue border-solid border-[0.25px] rounded hover:bg-light-grayish-blue-1 hover:border-transparent">
+    <p className="p-4 border-grayish-blue border-solid border-[0.25px] rounded hover:bg-light-grayish-blue-1 cursor-pointer hover:border-transparent">
       {props.message}
     </p>
   )
