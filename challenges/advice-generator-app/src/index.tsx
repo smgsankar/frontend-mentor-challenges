0c3ba703090ec2c9fba4d4b542b04fc56ec2dda5
smgsankar <gomzsankar@gmail.com>
2023-04-29 14:53:19 +0530

    fix(common): wrap app in strictmode

--- challenges/advice-generator-app/src/index.tsx
+++ challenges/advice-generator-app/src/index.tsx
@@ -1,5 +1,10 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
 const container = document.getElementById("root") as HTMLElement;
-createRoot(container).render(<App />);
+createRoot(container).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
