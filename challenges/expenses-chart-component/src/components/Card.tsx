96f0e847fe5dbd5f1a6a60bdd88869cc588601af
smgsankar <gomzsankar@gmail.com>
2023-04-29 12:16:03 +0530

    fix(expenses-chart): prettify card component

--- challenges/expenses-chart-component/src/components/Card.tsx
+++ challenges/expenses-chart-component/src/components/Card.tsx
@@ -4,10 +4,15 @@ interface CardPropsType {
   background: string;
 }
 
-export function Card({ background, children }: PropsWithChildren<CardPropsType>) {
+export function Card({
+  background,
+  children,
+}: PropsWithChildren<CardPropsType>) {
   return (
-    <div className={`bg-${background} w-[360px] md:w-[540px] my-4 mx-2 p-6 md:p-8 rounded-2xl`}>
+    <div
+      className={`bg-${background} w-[360px] md:w-[540px] my-4 mx-2 p-6 md:p-8 rounded-2xl`}
+    >
       {children}
     </div>
-  )
+  );
 }
